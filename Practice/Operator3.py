32cc45df98db8cb3ae7d64840ac59107d931cef3
CBR2026612 <CBR2026612@s.scnu.ac.kr>
2026-06-22 14:56:31 +0900

    너무 졸리고 피곤하다..

---
+++ Practice/Operator3.py
@@ -0,0 +1,17 @@
+# 너무 졸리고 피곤하다.
+print(2 + 4 * 2) # 10
+print((2 + 4) * 2) # 12
+number = 2 + 4 * 2 # 10
+print(number)
+number = number + 1 # 11
+# 근데 알지, 선언해줬으면 그 밑에서 다시 print문 써줘야 해..
+print(number)
+# 이거 c언어에서도 했었잖아.
+number += 7 # 18
+print(number)
+number /= 2 # 9
+print(number)
+number *= 202 # 1818
+print(number)
+number %= 5 # 3
+print(number)
---
+++ Practice/Practice3.py
@@ -0,0 +1,9 @@
+# 참 / 거짓
+print(5>10)
+print(5<10)
+print(True)
+# bool은 황소가 아니라 참 거짓 그런 거야?
+print(False)
+# NOT연산, 이건 c언어에서 !였는데. 아닌가?
+print(not False)
+print(not(5>10))
